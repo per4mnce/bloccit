329bef1cfd16fca63e4fdc92b79e94cad57fecb4
Henry Schaumburger <per4mnce@gmail.com>
2015-09-15 14:07:20 +0000

    Checkpoint 40 completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,8 @@
 class TopicsController < ApplicationController
+  
+  before_action :require_sign_in, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
+  
   def index
     @topics = Topic.all
   end
@@ -28,8 +32,7 @@ class TopicsController < ApplicationController
  
  def update
      @topic = Topic.find(params[:id])
-     @topic = Topic.new(topic_params) 
- 
+     @topic.assign_attributes(topic_params)
      if @topic.save
         flash[:notice] = "Topic was updated."
        redirect_to @topic
@@ -54,6 +57,13 @@ class TopicsController < ApplicationController
    private
    def topic_params
      params.require(:topic).permit(:name, :description, :public)
+   end
+   
+   def authorize_user
+    unless current_user.admin?
+      flash[:error] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
    end
  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
    has_many :posts
    before_save { self.email = email.downcase }
+   before_save { self.role ||= :member }
 
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-   validates :password, presence: true, length: { minimum: 6 }
+   #validates :password, presence: true, length: { minimum: 6 }
    validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
@@ -13,4 +14,6 @@ class User < ActiveRecord::Base
              format: { with: EMAIL_REGEX }
 
    has_secure_password
+   
+   enum role: [:member, :admin]
 end
