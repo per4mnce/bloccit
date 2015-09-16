d6bfe58b8562a5f3eb0d3d49888f1b2b7b528b0a
Henry Schaumburger <per4mnce@gmail.com>
2015-09-16 16:38:49 +0000

    Checkpoint 42 completed, labeling

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,7 +5,8 @@
    let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
  
    it { should have_many(:posts) }
- # #1
+   it { should have_many(:labelings) }
+   it { should have_many(:labels).through(:labelings) }
    describe "attributes" do
      it "should respond to name" do
        expect(topic).to respond_to(:name)
@@ -19,7 +20,6 @@
        expect(topic).to respond_to(:public)
      end
  
- # #2
      it "should be public by default" do
        expect(topic.public).to be(true)
      end
