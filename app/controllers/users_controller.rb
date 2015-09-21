70f6d40e43080b7056d731b737b837ee1727efb9
Henry Schaumburger <per4mnce@gmail.com>
2015-09-21 14:25:18 +0000

    Checkpoint 45 completed, public profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   def new
      @user = User.new
-   end
+  end
    
   def create
     @user = User.new
@@ -19,6 +19,11 @@ class UsersController < ApplicationController
       flash[:error] = "There was an error creating your account. Please try again."
       render :new
     end
+  end
+  
+  def show
+     @user = User.find(params[:id])
+     @posts = @user.posts.visible_to(current_user)
   end
   
 end
